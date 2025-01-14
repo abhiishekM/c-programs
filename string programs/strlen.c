4021f4662e6306fdb844dc02aba0aab2667091d4
abhiishekM <abhishivam285@gmail.com>
2025-01-14 10:09:45 +0530

    Create strlen.c

---
+++ string programs/strlen.c
@@ -0,0 +1,12 @@
+//program to calculate length of string
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[] = "C Programming";
+    
+    printf("Length of string %ld\n",strlen(str));
+
+    return 0;
+}
