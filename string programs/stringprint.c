2966a84e487407dcd9e3de358322205bafae852c
abhiishekM <abhishivam285@gmail.com>
2025-01-14 10:09:28 +0530

    Create stringprint.c

---
+++ string programs/stringprint.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//program to print a string
+int main()
+{
+    char sujeet[10]={'S','u','j','e','e','t','\0'};
+    printf("4th Character = %c\n",sujeet[3]);
+
+    return 0;
+}
