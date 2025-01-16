fd88249a6b9ef2d10fd7cc16c674de6833d70396
abhiishekM <abhishivam285@gmail.com>
2025-01-16 22:32:27 +0530

    Create malloc.c

---
+++ malloc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int n;
+	printf("Enter no. of Elements ");
+	scanf("%d",&n);
+	int *ptr;
+	ptr=(int*)malloc(n*sizeof(int));
+	printf("Enter Elements \n");
+	for(int i=0; i<n; ++i) {
+		scanf("%d",ptr+i);// we don't need to use & address operator here as initial address is already stored in pointer
+	}
+
+	for(int i=0; i<n; ++i) {
+		printf("%d ",*(ptr+i));
+	}
+
+	return 0;
+}
