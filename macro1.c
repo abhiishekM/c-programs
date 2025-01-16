21ee8d07563dd7af66b8e10b1762852d1a3992cb
abhiishekM <abhishivam285@gmail.com>
2025-01-16 22:33:24 +0530

    Create macro1.c

---
+++ macro1.c
@@ -0,0 +1,17 @@
+// C program to illustrate macros
+#include <stdio.h>
+
+// Macro definition
+#define INSTAGRAM FOLLOWERS
+#define FOLLOWERS A
+#define A b
+#define b 99
+
+// Driver Code
+int main()
+{
+	// Print the message
+	printf("Geeks for Geeks have %dK followers on Instagram",INSTAGRAM);
+
+	return 0;
+}
